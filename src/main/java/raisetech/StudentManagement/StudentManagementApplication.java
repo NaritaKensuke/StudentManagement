11a9b6a1f2d54a79471acc42b85ec5d847ad8680
Narita Kensuke <naritak2024@outlook.jp>
2024-10-03 23:23:59 +0900

    生徒の名前と年齢を表示するシステム

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,9 +1,11 @@
 package raisetech.StudentManagement;
 
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,13 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class
 StudentManagementApplication {
 
+	private String name = "Narita Kensuke";
+	private String age = "24";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello,World!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + " " + age + "歳";
 	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age){
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
+
+	@PostMapping("/studentAge")
+	public void updateStudentAge(String age){
+		this.name = age;
+	}
+
 
 }
