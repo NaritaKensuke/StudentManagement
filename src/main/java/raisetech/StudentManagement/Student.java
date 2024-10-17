5e953aa326888fbeaaa7388ac7f4d94394cc936e
Narita Kensuke <naritak2024@outlook.jp>
2024-10-17 21:50:32 +0900

    受講生情報の一括取得

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,23 +1,19 @@
 package raisetech.StudentManagement;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
 
+  private String studentId;
   private String name;
+  private String nameReading;
+  private String nickname;
+  private String mailAddress;
+  private String city;
   private int age;
+  private String gender;
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
 }
--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,23 +1,15 @@
 package raisetech.StudentManagement;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
-
-	private List<String> names = new ArrayList<>();
 
 	@Autowired
 	private StudentRepository repository;
@@ -26,31 +18,9 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/student")
-	public Student getStudent(@RequestParam String name) {
-		Student student = repository.searchByName(name);
-		return student;
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
 	}
-
-	@GetMapping("/students")
-	public List<Student> getStudents() throws SQLException {
-		return repository.selectAll();
-	}
-
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudent(String name, int age){
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
-
 
 }
