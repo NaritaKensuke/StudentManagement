84de2afcb7d0eef250e6a8b6b8b1a1b669977f6a
Narita Kensuke <naritak2024@outlook.jp>
2024-10-19 16:09:39 +0900

    受講生情報とコース情報の検索条件変更

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.StudentManagement.data.Student;
@@ -18,10 +19,25 @@ public class StudentService {
   }
 
   public List<Student> searchStudentList() {
-    return repository.searchStudent();
+    //全件検索
+    List<Student> students = repository.searchStudent();
+
+    //絞り込み検索で30代の人を抽出
+    List<Student> filterStudentsList = students.stream()
+        .filter(student -> 30 <= student.getAge() && student.getAge() < 40)
+        .collect(Collectors.toList());
+
+    //抽出したリストをコントローラーに返す
+    return filterStudentsList;
   }
 
-  public List<StudentCourse> searchStudentsCourseList(){
-    return repository.searchStudentCourse();
+  public List<StudentCourse> searchStudentsCoursesList(){
+    //絞り込み検索で「Javaコース」のコース情報のみを抽出する
+    List<StudentCourse> filterStudentsCoursesList = repository.searchStudentsCourses().stream()
+        .filter(studentsCourses -> studentsCourses.getCourseName().equals("Java"))
+        .collect(Collectors.toList());
+
+    //抽出したリストをコントローラーに返す
+    return filterStudentsCoursesList;
   }
 }
