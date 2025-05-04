3d0d07b09da0e6d5381b5af83c94554ec6cf2742
Narita Kensuke <naritak2024@outlook.jp>
2025-05-04 18:12:40 +0900

    エラーメッセージをJSONで返す

--- src/main/java/raisetech/StudentManagement/controller/handler/ControllerExceptionHandler.java
+++ src/main/java/raisetech/StudentManagement/controller/handler/ControllerExceptionHandler.java
@@ -4,13 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import raisetech.StudentManagement.exception.ExceptionMessage;
 import raisetech.StudentManagement.exception.TestException;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
   @ExceptionHandler(TestException.class)
-  public ResponseEntity<String> handlerTestException(TestException ex){
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  public ResponseEntity<ExceptionMessage> handlerTestException(TestException ex){
+    ExceptionMessage exceptionMessage = new ExceptionMessage(ex.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionMessage);
   }
+
 }
