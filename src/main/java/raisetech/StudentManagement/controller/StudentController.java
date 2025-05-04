b540e62c0e29ba14018dc9a6ee2afe1efadf452e
Narita Kensuke <naritak2024@outlook.jp>
2025-05-05 00:59:31 +0900

    OpenAPI実装

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,5 +1,8 @@
 package raisetech.StudentManagement.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -41,6 +44,8 @@ public class StudentController {
    * @param deleted 論理削除の情報を受け取る
    * @return 論理削除がtrueの受講生リストもしくはfalseの受講生リストを返す
    */
+  @Operation(summary = "受講生一覧",description = "すべての受講生の基本情報一覧を検索する")
+  @Parameter(description = "論理削除")
   @GetMapping("/studentList")
   public List<Student> getStudentList(@RequestParam("deleted") boolean deleted){
       return service.searchStudentList(deleted);
@@ -53,6 +58,8 @@ public class StudentController {
    * @return
    * @throws TestException エラーを返す
    */
+  @Operation(summary = "未使用",description = "未使用のAPI")
+  @ApiResponse(responseCode = "400",description = "未使用のAPI")
   @GetMapping("/students")
   public List<Student> getStudents() throws TestException {
     throw new TestException("こちらのAPIは現在使用することができません。URLをご確認ください。");
@@ -64,6 +71,8 @@ public class StudentController {
    * @param deleted 論理削除の情報を受け取る
    * @return 論理削除がtrueの受講生コースリストもしくはfalseの受講生コースリストを返す
    */
+  @Operation(summary = "コース情報一覧",description = "すべての受講生のコース情報一覧を検索する")
+  @Parameter(description = "論理削除")
   @GetMapping("/allStudentCourseList")
   public List<StudentCourse> getAllStudentsCourseList(
       @RequestParam("deleted") boolean deleted){
@@ -76,6 +85,10 @@ public class StudentController {
    * @param studentId 受講生IDの情報を受け取る
    * @return 受け取った受講生IDを持つ受講生コース情報を返す
    */
+  @Operation(summary = "受講生コース情報一覧",description = "特定の受講生のコース情報一覧を検索する")
+  @Parameter(description = "受講生ID")
+  @ApiResponse(responseCode = "200",description = "成功")
+  @ApiResponse(responseCode = "400",description = "クエリパラメータの入力エラー")
   @GetMapping("/studentCourseList")
   public List<StudentCourse> getStudentCourseList(
       @RequestParam("studentId") @Size(min = 1,max = 3) String studentId){
@@ -88,6 +101,8 @@ public class StudentController {
    * @param studentDetail 登録する受講生情報を受け取る
    * @return 正常に処理された場合、論理削除がfalseの受講生情報リストを返す
    */
+  @Operation(summary = "受講生登録",description = "受講生情報を登録する")
+  @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "受講生情報")
   @PostMapping("/registerStudent")
   public ResponseEntity<List> registerStudent(
       @RequestBody StudentDetail studentDetail){
@@ -101,6 +116,8 @@ public class StudentController {
    * @param studentDetail 更新する受講生の基本情報を受け取る
    * @return 正常に処理された場合、更新した受講生の基本情報を返す
    */
+  @Operation(summary = "受講生基本情報更新",description = "受講生の基本情報を更新する")
+  @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "更新する受講生情報")
   @PutMapping("/updateStudent")
   public ResponseEntity<Student> updateStudent(@RequestBody StudentDetail studentDetail){
     service.updateStudent(studentDetail);
@@ -113,9 +130,14 @@ public class StudentController {
    * @param studentDetail 更新する受講生のコース情報を受け取る
    * @return 正常に処理された場合、更新した受講生のコース情報を受け取る
    */
+  @Operation(summary = "受講生コース情報更新",description = "受講生のコース情報を更新する")
+  @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "更新する受講生情報")
+  @ApiResponse(responseCode = "200",description = "成功")
+  @ApiResponse(responseCode = "400",description = "更新情報の不足")
   @PutMapping("/updateStudentCourse")
   public StudentCourse updateStudentCourse(@RequestBody @Valid StudentDetail studentDetail){
     service.updateStudentCourse(studentDetail);
     return service.searchStudentCourse(studentDetail);
   }
+
 }
--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 
+@Schema(description = "受講生情報")
 @Getter
 @Setter
 public class StudentDetail {
