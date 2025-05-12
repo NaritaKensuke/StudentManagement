88e64585436e0808685fb4bd8582fa5f4ca8c348
Narita Kensuke <naritak2024@outlook.jp>
2025-05-12 23:10:17 +0900

    Converterの自動テスト

--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -3,7 +3,9 @@ package raisetech.StudentManagement.domain;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
@@ -11,6 +13,8 @@ import raisetech.StudentManagement.data.StudentCourse;
 @Schema(description = "受講生情報")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDetail {
 
   @Valid
